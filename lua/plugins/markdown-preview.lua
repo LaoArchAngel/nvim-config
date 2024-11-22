0ad3df7c62e78b85f18fd693ed5a330fa213eea2
Malick McGregor <malick@turncommerce.com>
2024-11-22 15:13:49 -0500

    Add markdown preview plugin

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview" },
+    build = function()
+      vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim")
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}
