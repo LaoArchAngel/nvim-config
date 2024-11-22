0f90acdbe4b0e26d0c9a376e7c4db2c21e28ac73
Malick McGregor <malick@turncommerce.com>
2024-11-22 17:47:08 -0500

    Personalize copy path ordering

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,13 +10,13 @@ return {
           local modify = vim.fn.fnamemodify
 
           local vals = {
-            ["BASENAME"] = modify(filename, ":r"),
-            ["EXTENSION"] = modify(filename, ":e"),
-            ["FILENAME"] = filename,
-            ["PATH (CWD)"] = modify(filepath, ":."),
-            ["PATH (HOME)"] = modify(filepath, ":~"),
-            ["PATH"] = filepath,
-            ["URI"] = vim.uri_from_fname(filepath),
+            ["1. PATH (CWD)"] = modify(filepath, ":."),
+            ["2. FILENAME"] = filename,
+            ["3. PATH"] = filepath,
+            ["4. BASENAME"] = modify(filename, ":r"),
+            ["5. EXTENSION"] = modify(filename, ":e"),
+            ["6. URI"] = vim.uri_from_fname(filepath),
+            ["7. PATH (HOME)"] = modify(filepath, ":~"),
           }
 
           local options = vim.tbl_filter(function(val)
