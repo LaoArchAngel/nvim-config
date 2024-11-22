3a8a56b6612dcf28b57729fb8ce2ea140d0da641
Malick McGregor <malick@turncommerce.com>
2024-11-22 17:02:38 -0500

    Try to disable some linters not installing

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["gitlab-ci-ls"] = {},
+        ["markdownlint-cli2"] = {},
+        ["markdown-toc"] = {},
+      },
+    },
+  },
+}
