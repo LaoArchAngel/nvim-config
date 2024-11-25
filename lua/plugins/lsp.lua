cb5e8401f5f744457fc3067f93029b2c3bfc7c06
Malick (Lao) McGregor <lao@nexverse.net>
2024-11-25 01:51:26 -0500

    Disable lua_ls @ AMUMSS folder

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          mason = true,
+          root_dir = function(fname)
+
+            local util = require("lspconfig.util");
+
+            local root_files = {
+              ".luarc.json",
+              ".luarc.jsonc",
+              ".luacheckrc",
+              ".stylua.toml",
+              "stylua.toml",
+              "selene.toml",
+              "selene.yml",
+            }
+            
+            if string.find(fname, "AMUMSS") then return nil end
+
+            local root = util.root_pattern(unpack(root_files))(fname)
+            if root and root ~= vim.env.HOME then
+              return root
+            end
+
+            local root_lua = util.root_pattern 'lua/'(fname) or ''
+            local root_git = util.find_git_ancestor(fname) or ''
+            if root_lua == '' and root_git == '' then
+              return
+            end
+            return #root_lua >= #root_git and root_lua or root_git
+          end,
+        },
+        dockerls = {
+          mason = false,
+        },
+        jsonls = {
+          mason = false,
+        },
+        yamlls = {
+          mason = false,
+        },
+        docker_compose_language_service = {
+          mason = false,
+        },
+      },
+    },
+  },
+}
